c7eea712443e366f0ca5c9524d61ba835b64dace
Ali Eren KÖSE <alierenkose@gmail.com>
2024-02-21 14:21:05 +0300

    hata düzeltme

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ public class Main {
     int a , b, c,sonuc;
 
     Scanner input = new Scanner(System.in);
-        System.out.print("(a+b)*(c-b) işlemi için a sayısını girin : ");
+        System.out.print("a+b*c-b işlemi için a sayısını girin : ");
         a = input.nextInt();
 
-       System.out.print("(a+b)*(c-b) işlemi için b sayısını girin: ");
+       System.out.print("(a+b*c-b işlemi için b sayısını girin: ");
        b = input.nextInt();
 
-        System.out.print("(a+b)*(c-b) işlemi için c sayısını girin: ");
+        System.out.print("a+b*c-b işlemi için c sayısını girin: ");
         c = input.nextInt();
 
-       sonuc=(a+b)*(c-b);
+       sonuc=a+b*c-b;           //
         System.out.println("İşlemin Sonucu : " + sonuc);
     }
 }       // Ali Eren KÖSE 21/02/2024 14:08
