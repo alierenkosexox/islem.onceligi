235fd583cdfd85693e886c8c905d12b031e720ca
Ali Eren KÖSE <alierenkose@gmail.com>
2024-02-21 14:22:56 +0300

    hata düzeltme ve yorum yazma

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-    int a , b, c,sonuc;
+    int a , b, c,sonuc;         // işlemdeki a,b,c, sonuc değerlerini tanımla
 
     Scanner input = new Scanner(System.in);
-        System.out.print("a+b*c-b işlemi için a sayısını girin : ");
+        System.out.print("a+b*c-b işlemi için a sayısını girin : ");    // a sayısın sor ve kaydet
         a = input.nextInt();
 
-       System.out.print("(a+b*c-b işlemi için b sayısını girin: ");
+       System.out.print("(a+b*c-b işlemi için b sayısını girin: ");        //b sayısını sor ve kaydet
        b = input.nextInt();
 
-        System.out.print("a+b*c-b işlemi için c sayısını girin: ");
+        System.out.print("a+b*c-b işlemi için c sayısını girin: ");         // c sor ve tanımla
         c = input.nextInt();
 
-       sonuc=a+b*c-b;           //
+       sonuc=a+b*c-b;           //alınan değerlerle işlemi yaptır ve yazıdır
         System.out.println("İşlemin Sonucu : " + sonuc);
     }
 }       // Ali Eren KÖSE 21/02/2024 14:08
